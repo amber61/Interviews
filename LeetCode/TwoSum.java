56bc2588658914e3d1f755cb5fcff00a5e0ca398
ZHE HUANG <huangzhe.ca@gmail.com>
2017-05-03 16:08:22 -0400

    Update TwoSum.java

--- LeetCode/TwoSum.java
+++ LeetCode/TwoSum.java
@@ -9,7 +9,7 @@ public class Solution {
     int[] indice;
     public int[] twoSum(int[] nums, int target) {
         for(int i = 0; i < nums.length - 1; i++) {
-            for (int j = 1; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
                     indice = new int[]{i, j};
                     //return indice; 
