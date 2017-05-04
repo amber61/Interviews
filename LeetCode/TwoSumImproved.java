8632d5b6065baf2f59fc1e48e0d13dcbbd38b5ad
amber61 <huangzhe.ca@gmail.com>
2017-05-04 17:31:40 -0400

    two sum improved using map

---
+++ LeetCode/TwoSumImproved.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public static int[] twoSum(int[] nums, int target) {
+	Map<Integer, Integer> map = new HashMap<>();
+
+	for (int i = 0; i < nums.length; i++) {
+	    map.put(nums[i], i);
+	}
+	for (int i = 0; i < nums.length; i++) {
+	    int complement = target - nums[i];
+	    if (map.containsKey(complement) && map.get(complement) != i) {
+		return new int[] { i, map.get(complement) };
+	    }
+	}
+	throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+	int[] ss = twoSum(new int[] { 3, 2, 4 }, 6);
+	for (int s : ss)
+	    System.out.print(s + " ");
+    }
+}
